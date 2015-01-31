141ffa190dd0330d89f9dac741dcc5ecafc0b52f
Aaryn Olsson <aaryno1@Aaryns-MacBook-Pro.local>
2015-01-31 03:20:07 -0600

    updates to readme and settings

--- src/main/java/aaryn/nmap/ws/Main.java
+++ src/main/java/aaryn/nmap/ws/Main.java
@@ -5,6 +5,8 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import aaryn.nmap.util.NmapWebProperties;
+
 import java.io.IOException;
 import java.net.URI;
 
@@ -14,7 +16,19 @@ import java.net.URI;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/";
+    public static final String BASE_URI;
+    static {
+
+		String port;
+		try {
+			port = NmapWebProperties.getInstance().getProperties().getProperty("grizzly_port");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Using port 8080");
+			port="8080";
+		}
+		BASE_URI= "http://localhost:"+port+"/";
+    }
     
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
--- src/main/resources/create_tables.sql
+++ src/main/resources/create_tables.sql
@@ -1,2 +1,97 @@
-create table scan_hosts (id int NOT NULL,
-hostname varchar(
+-- MySQL dump 10.13  Distrib 5.6.22, for osx10.8 (x86_64)
+--
+-- Host: localhost    Database: nmapscans
+-- ------------------------------------------------------
+-- Server version	5.6.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `HostAlias`
+--
+
+DROP TABLE IF EXISTS `HostAlias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `HostAlias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fqdn` varchar(255) DEFAULT NULL,
+  `internetHostId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK4B07196856715D7F` (`internetHostId`),
+  CONSTRAINT `FK4B07196856715D7F` FOREIGN KEY (`internetHostId`) REFERENCES `InternetHost` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `InternetHost`
+--
+
+DROP TABLE IF EXISTS `InternetHost`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `InternetHost` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(255) DEFAULT NULL,
+  `ipType` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ip` (`ip`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `NmapScan`
+--
+
+DROP TABLE IF EXISTS `NmapScan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `NmapScan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `error` varchar(255) DEFAULT NULL,
+  `hostFound` bit(1) DEFAULT NULL,
+  `scanDate` datetime DEFAULT NULL,
+  `internetHostId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKD87A7DEB56715D7F` (`internetHostId`),
+  CONSTRAINT `FKD87A7DEB56715D7F` FOREIGN KEY (`internetHostId`) REFERENCES `InternetHost` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ScanPort`
+--
+
+DROP TABLE IF EXISTS `ScanPort`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ScanPort` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `port` int(11) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `nmapScanId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKCEC4B0BE1671A083` (`nmapScanId`),
+  CONSTRAINT `FKCEC4B0BE1671A083` FOREIGN KEY (`nmapScanId`) REFERENCES `NmapScan` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=165 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-01-31  2:51:06
